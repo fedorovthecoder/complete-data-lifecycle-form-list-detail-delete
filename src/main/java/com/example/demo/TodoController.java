8ab46631e21db55dbe25b8374ad7d091b061a5f7
Federico Carbonell <fed.carbonell@gmail.com>
2019-03-12 16:12:29 -0400

    Started exercise 304.

---
+++ src/main/java/com/example/demo/TodoController.java
@@ -0,0 +1,58 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class TodoController {
+
+    @Autowired
+    TodoRepository todoRepository;
+
+    @RequestMapping("/list")
+    public String listTodos(Model model) {
+        model.addAttribute("todos", todoRepository.findAll());
+        return "todolist";
+    }
+
+    @GetMapping("/addtodo")
+    public String todoForm(Model model) {
+        model.addAttribute("todo", new Todo());
+        return "todoform";
+    }
+
+    @PostMapping("/processtodo")
+    public String processForm(@Valid Todo todo, BindingResult result) {
+        if (result.hasErrors()) {
+            return "todoform";
+        }
+        todoRepository.save(todo);
+        return ("redirect:/list");
+    }
+
+    @RequestMapping("/todo-detail/{id}")
+    public String showTodo(@PathVariable("id")  long id, Model model) {
+        model.addAttribute("todo", todoRepository.findById(id).get());
+        return "showtodo";
+    }
+
+    @RequestMapping("/todo-update/{id}")
+    public String updateTodo(@PathVariable("id")  long id, Model model) {
+        model.addAttribute("todo", todoRepository.findById(id).get());
+        return "todoform";
+    }
+
+    @RequestMapping("/todo-delete/{id}")
+    public String delTodo(@PathVariable("id")  long id) {
+        todoRepository.deleteById(id);
+        return "redirect:/list";
+    }
+}
